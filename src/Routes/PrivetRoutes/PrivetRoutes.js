02d612e1f1f5bb0ef2d6ef8f94be27d21493a317
unknown <mahamuda3052@gmail.com>
2022-11-09 00:50:40 +0600

    create privete route

--- src/Routes/PrivetRoutes/PrivetRoutes.js
+++ src/Routes/PrivetRoutes/PrivetRoutes.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { authContext } from '../../Context/AuthProvider/AuthProvider';
 
-const PrivetRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivetRoutes = ({children}) => {
+    const{user,loader}=useContext(authContext)
+    const location =useLocation()
+    if(loader){
+        return <>
+        <button type="button" class="bg-indigo-500 ..." disabled>
+  <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+   
+  </svg>
+  Processing...
+</button>
+        </>
+    }
+    if(!user){
+        return<Navigate to='/login' state={{from:location}} replace></Navigate>
+    }
+    return children
 };
 
 export default PrivetRoutes;
